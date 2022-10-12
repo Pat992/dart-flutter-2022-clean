ad973271dd42be9ac1827e9943ba566b212d8c28
Patrick Hettich <patrick_hettich@hotmail.com>
2022-10-12 21:07:30 +0200

    added indexedStack, keeps state of widgets

---
+++ flutter_grundlagen/lib/root.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_grundlagen/presentation/counter_app/counter_app_page.dart';
+import 'package:flutter_grundlagen/presentation/widgets_examples/widgets_examples_page.dart';
+
+class RootWidget extends StatefulWidget {
+  const RootWidget({Key? key}) : super(key: key);
+
+  @override
+  State<RootWidget> createState() => _RootWidgetState();
+}
+
+class _RootWidgetState extends State<RootWidget> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // VERY INTERESTING, keeps state while moving between pages
+      body: IndexedStack(
+        index: _index,
+        children: const <Widget>[
+          WidgetsExamplesPage(),
+          CounterAppPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => setState(() {
+          _index = index;
+        }),
+        currentIndex: _index,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.blue,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Examples',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Counter App',
+          ),
+        ],
+      ),
+    );
+  }
+}
