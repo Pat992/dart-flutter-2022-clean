5e1e5116fe6d453730f3afd1822bbb187b26cfb7
Patrick Hettich <patrick_hettich@hotmail.com>
2022-10-12 16:53:24 +0200

    container and text done

---
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets/container_text_example.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ContainerTextExample extends StatelessWidget {
+  const ContainerTextExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          border: Border.all(color: Colors.black, width: 4),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        height: 180,
+        width: 300,
+        alignment: Alignment.center,
+        child: Material(
+          borderRadius: BorderRadius.circular(10),
+          elevation: 10,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            height: 100,
+            width: 200,
+            alignment: Alignment.center,
+            child: const Text(
+              'Example Text',
+              style: TextStyle(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
+++ flutter_grundlagen/lib/presentation/widgets_examples/widgets_examples_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_grundlagen/presentation/widgets_examples/widgets/container_text_example.dart';
 
 class WidgetsExamplesPage extends StatelessWidget {
   const WidgetsExamplesPage({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class WidgetsExamplesPage extends StatelessWidget {
         leading: const Icon(Icons.home),
         title: const Text('Widgets Example'),
       ),
-      body: const Center(),
+      body: const ContainerTextExample(),
     );
   }
 }
