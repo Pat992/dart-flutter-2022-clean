68b2facd5d3587badafb24754c050a4ba717e90a
Patrick Hettich <patrick_hettich@hotmail.com>
2022-10-17 15:09:49 +0200

    theme integrated in application

--- adviser/lib/main.dart
+++ adviser/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:adviser/application/adviser/adviser_bloc.dart';
+import 'package:adviser/application/theme/theme_service_provider.dart';
 import 'package:adviser/presentation/adviser/adviser_page.dart';
 import 'package:adviser/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:adviser/injection.dart' as injection;
 import 'package:adviser/injection.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   // Only start app after dependency injection
   WidgetsFlutterBinding.ensureInitialized();
   // Run dependency injection
   await injection.init();
-  runApp(const MyApp());
+  // Run init function of the Provider
+  injection.getIt<ThemeServiceImpl>().init();
+  runApp(ChangeNotifierProvider(
+    create: (context) => injection.getIt<ThemeServiceImpl>(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,13 +26,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.darkTheme,
-      title: 'Adviser',
-      home: BlocProvider(
-        create: (context) => getIt<AdviserBloc>(),
-        child: const AdviserPage(),
-      ),
+    return Consumer<ThemeServiceImpl>(
+      builder: (context, themeService, child) {
+        return MaterialApp(
+          theme: themeService.isDarkMode
+              ? AppTheme.darkTheme
+              : AppTheme.lightTheme,
+          title: 'Adviser',
+          home: BlocProvider(
+            create: (context) => getIt<AdviserBloc>(),
+            child: const AdviserPage(),
+          ),
+        );
+      },
     );
   }
 }
--- adviser/lib/presentation/adviser/adviser_page.dart
+++ adviser/lib/presentation/adviser/adviser_page.dart
@@ -1,9 +1,11 @@
 import 'package:adviser/application/adviser/adviser_bloc.dart';
+import 'package:adviser/application/theme/theme_service_provider.dart';
 import 'package:adviser/presentation/adviser/widgets/advise_field.dart';
 import 'package:adviser/presentation/adviser/widgets/custom_button.dart';
 import 'package:adviser/presentation/adviser/widgets/error_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 class AdviserPage extends StatelessWidget {
   const AdviserPage({Key? key}) : super(key: key);
@@ -14,6 +16,14 @@ class AdviserPage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Adviser', style: Theme.of(context).textTheme.headline1),
+        actions: [
+          Switch(
+            value: Provider.of<ThemeServiceImpl>(context).isDarkMode,
+            onChanged: (_) =>
+                Provider.of<ThemeServiceImpl>(context, listen: false)
+                    .toggleTheme(),
+          ),
+        ],
       ),
       body: Center(
         child: Padding(
