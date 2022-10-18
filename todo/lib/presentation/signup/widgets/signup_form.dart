7e2a70a91f03ddab16df1e16776d4f06ab663ef3
Patrick Hettich <patrick_hettich@hotmail.com>
2022-10-18 16:43:18 +0200

    added input validation

--- todo/lib/presentation/signup/widgets/signup_form.dart
+++ todo/lib/presentation/signup/widgets/signup_form.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:todo/presentation/general_widgets/custom_button.dart';
+import 'package:todo/presentation/core/custom_button.dart';
 
 class SignupForm extends StatelessWidget {
   const SignupForm({Key? key}) : super(key: key);
 
+  String? validateEmail(String? input) {
+    const emailRegex =
+        r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+
+    if (input == null || input.isEmpty) {
+      return 'Please enter an E-Mail';
+    } else if (RegExp(emailRegex).hasMatch(input)) {
+      return null;
+    }
+    return 'Please enter a valid E-Mail';
+  }
+
+  String? validatePassword(String? input) {
+    if (input == null || input.isEmpty) {
+      return 'Please enter a Password';
+    } else if (input.length >= 6) {
+      return null;
+    }
+    return 'Please enter a Password with at least 6 characters';
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
     return Form(
+      key: formKey,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: ListView(
@@ -35,19 +58,59 @@ class SignupForm extends StatelessWidget {
             ),
             const SizedBox(height: 80),
             TextFormField(
+              validator: validateEmail,
+              autovalidateMode: AutovalidateMode.disabled,
               cursorColor: themeData.textTheme.headline1?.color,
               decoration: const InputDecoration(label: Text('E-Mail')),
             ),
             const SizedBox(height: 20),
             TextFormField(
+              validator: validatePassword,
+              autovalidateMode: AutovalidateMode.disabled,
               cursorColor: themeData.textTheme.headline1?.color,
               obscureText: true,
               decoration: const InputDecoration(label: Text('Password')),
             ),
             const SizedBox(height: 40),
-            CustomButton(text: 'Sign in', callback: () => print('sign in')),
+            CustomButton(
+              text: 'Sign in',
+              callback: () {
+                if (formKey.currentState!.validate()) {
+                  print('sign in');
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      backgroundColor: Colors.redAccent,
+                      content: Text(
+                        'Invalid inputs',
+                        style: themeData.textTheme.bodyText1,
+                      ),
+                    ),
+                  );
+                  print('invalid');
+                }
+              },
+            ),
             const SizedBox(height: 20),
-            CustomButton(text: 'Register', callback: () => print('register')),
+            CustomButton(
+              text: 'Register',
+              callback: () {
+                if (formKey.currentState!.validate()) {
+                  print('register');
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      backgroundColor: Colors.redAccent,
+                      content: Text(
+                        'Invalid inputs',
+                        style: themeData.textTheme.bodyText1,
+                      ),
+                    ),
+                  );
+                  print('invalid');
+                }
+              },
+            ),
           ],
         ),
       ),
