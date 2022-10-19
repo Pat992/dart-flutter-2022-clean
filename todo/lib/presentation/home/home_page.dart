2d42a04a66a87a0613421d331b20f1df856fc539
Patrick Hettich <patrick_hettich@hotmail.com>
2022-10-19 20:37:32 +0200

    bloc controller added

--- todo/lib/presentation/home/home_page.dart
+++ todo/lib/presentation/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/application/auth/auth_bloc.dart';
-import 'package:todo/application/todo/observer_bloc.dart';
+import 'package:todo/application/todo/observer/observer_bloc.dart';
 import 'package:todo/injection.dart';
 import 'package:todo/presentation/home/widgets/home_body.dart';
 import 'package:todo/presentation/routes/router.gr.dart';
