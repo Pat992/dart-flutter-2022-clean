3af342ee320854af41f016d0ff97aa03115ba347
Patrick Hettich <patrick_hettich@hotmail.com>
2022-10-20 21:36:58 +0200

    init responsiveFramework

--- flutter_web/lib/main.dart
+++ flutter_web/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web/presentation/homepage/home_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Web',
-      home: HomePage(),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        widget,
+        defaultScale: true,
+        minWidth: 400,
+        defaultName: MOBILE,
+        // to set free space, in case it should happen
+        backgroundColor: Colors.white,
+        // Set breakpoints for later scaling
+        breakpoints: const [
+          ResponsiveBreakpoint.autoScale(450, name: MOBILE),
+          ResponsiveBreakpoint.resize(600, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+      ),
+      home: const HomePage(),
     );
   }
 }
